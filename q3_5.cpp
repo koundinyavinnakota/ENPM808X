37ec243713688eccc5d8c42e331b91d96f6d5eb0
koundinyavinnakota <koundinya.vinnakota@gmail.com>
2022-09-18 16:54:34 -0400

    Final version of q3_5.cpp

--- q3_5.cpp
+++ q3_5.cpp
@@ -15,39 +15,58 @@ using std::setprecision;
 using std::streamsize;
 
 
-void computeGrade(double midterm,double final,double median){
+double computeGrade(double midterm,double final,double median){
     streamsize prec = cout.precision();
     cout << "Your final grade is " << setprecision(3)
         << 0.2 * midterm + 0.4 * final + 0.4 * median
         << setprecision(prec) << endl;
-
+    return 0.2 * midterm + 0.4 * final + 0.4 * median;
 }
+
+void classGrades(vector<string> &names,vector<double> finalGrades ){
+     cout<<"***************** Final Grades of students ********************"<<endl;
+    for(int i = 0; i<names.size();i++){
+       
+        cout<<"      "<<names[i]<<"  -  "<<finalGrades[i]<<endl;
+    }
+}
+
 int main(){
+    vector<string> names;
+    vector<double> finalGrades;
+    int counter_1 = 0;
+    while(counter_1<5){
+        cout<< "Please enter your name !!!"<< endl;
+        string name;
+        cin>>name;
+        names.push_back(name);
+        cout<<" Hi "<< name<<" nice to meet you !!"<<endl;
+        cout<<" Please enter you midterm and final grades: "<<endl;
+        double midterm, final;
+        cin>>midterm>>final;
+        cout<<"Enter five of your home work grades :"<<endl;
+        vector<double> homework;
+        double temp;
+        int counter_2 = 0;
+        while(counter_2<5){
+            cin>>temp;
+            homework.push_back(temp);
+            counter_2++;
+        }
+        typedef vector<double>::size_type vector_size;
+        vector_size size = homework.size();
+        if(size == 0){
+            cout<<" You must enter atleast one homework grade to calcuate your final grade "<<endl;
+            return 1;
+        }
 
-    cout<< "Please enter your name !!!"<< endl;
-    string name;
-    cin>>name;
-    cout<<" Hi "<< name<<" nice to meet you !!"<<endl;
-    cout<<" Please enter you midterm and final grades: "<<endl;
-    double midterm, final;
-    cin>>midterm>>final;
-    cout<<"Enter all your home work grades :"<<endl;
-    vector<double> homework;
-    double temp;
-    while(cin>>temp){
-        homework.push_back(temp);
+        sort(homework.begin(),homework.end());
+        double median;
+        median = size % 2 == 0 ? (homework[size/2] + homework[(size/2)-1]) / 2
+                                    : homework[size/2];
+        finalGrades.push_back(computeGrade(midterm, final,  median));
+        counter_1++;
     }
-    typedef vector<double>::size_type vector_size;
-    vector_size size = homework.size();
-    if(size == 0){
-        cout<<" You must enter grades to calcuate your final grade "<<endl;
-        return 1;
-    }
-
-    sort(homework.begin(),homework.end());
-    double median;
-    median = size % 2 == 0 ? (homework[size/2] + homework[(size/2)-1]) / 2
-                                : homework[size/2];
-    computeGrade(midterm, final,  median);
+    classGrades(names,  finalGrades);
     return 0;
 }
