9d1b04fe1c840053e1371fd78493a4c1048bb06f
koundinyavinnakota <koundinya.vinnakota@gmail.com>
2022-09-19 01:12:08 -0400

    final edits to q4_5.cpp

--- q4_5.cpp
+++ q4_5.cpp
@@ -11,10 +11,18 @@ using std::endl;
 using std::string;
 using std::vector;
 using std::streamsize;
-
+/**
+ * @brief Function to read words from the input stream into a vector.
+ * 
+ * @param word_list - reference of the word_list from MAIN function
+ * @return auto -  It doesn't return anything, but the word_list is modified because the paramter is referenced.
+ */
 auto ReadWords(vector<string>&word_list){
     cout<<" Start entering words"<<endl;
     string temp;
+    //function to input values from user.
+    // When finished entering values press "Ctrl + D"
+    //This signifies end of terminal line input for linux
     while(cin>>temp){
         word_list.push_back(temp);
 
@@ -25,12 +33,19 @@ auto ReadWords(vector<string>&word_list){
         cout<<word_list[i]<<endl;
     }
 }
+/**
+ * @brief Function to count the number of words and number of repititions
+ * 
+ * @param list - a copy of the original list of words entered by user
+ * @return auto - No return values instead the function prints the number of words and repititon of words.
+ */
 auto CountNumberOfWords(vector<string> list)
 {
     int counter = list.size();
     
     string temp;
     vector<int> match_counter;
+    //Iterating through loops to find the number of times each word has been repeated.
     for(auto it = 0;it<list.size();it++)
     {
         int temp = 0;
@@ -42,14 +57,15 @@ auto CountNumberOfWords(vector<string> list)
            
                 
         }
-      
+        // matching counter, holds the repitition of words. 
         match_counter.push_back(temp);
         
     }
         
-    
+    // total number of words entered by the user.
     cout<<" Number of words in the list : "<<counter<<endl;
     cout<<" Number of times each word occured "<<endl;
+    //Statement to print the list of words with their instances
     for(auto iterator = 0;iterator<list.size();iterator++){
         cout<<list[iterator]<<" - "<<match_counter[iterator]<<endl;
     }
@@ -58,7 +74,9 @@ auto CountNumberOfWords(vector<string> list)
 
 int main(){
     vector<string> word_list;
+    //function call 
     ReadWords(word_list);
+    //function call
     CountNumberOfWords(word_list);
     return 0;
 }
